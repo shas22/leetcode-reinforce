87bc33409e0105994f02c77429b0bee5b6975039
shas22 <shas@apollocode.io>
2024-09-09 23:50:13 +0800

    solution for 287

---
+++ patterns/binary-tree/impl.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, left=None, right=None, val):
+        self.left = left
+        self.right = right
+        self.val = val
+        
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+    
+    
