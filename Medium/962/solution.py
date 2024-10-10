86a3a7226ebe514bec696927061c2b1c81635dee
prodsl <imptshas@outlook.com>
2024-10-10 17:06:28 +0800

    daily sol 962

---
+++ Medium/962/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        n = len(nums)
+        stack = []
+        
+        for i in range(n):
+            if not stack or nums[stack[-1]] > nums[i]:
+                stack.append(i)
+        
+        max_width = 0
+        for i in range(n - 1, -1, -1):
+            while stack and nums[stack[-1]] <= nums[i]:
+                max_width = max(max_width, i - stack.pop())
+        
+        return max_width
