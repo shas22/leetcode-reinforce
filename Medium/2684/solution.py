7f525dcb8b3a31d47167013992398af56ded1b33
prodsl <imptshas@outlook.com>
2024-10-29 22:09:57 +0800

    daily sol 2684 update

--- Medium/2684/solution.py
+++ Medium/2684/solution.py
@@ -1,15 +1,22 @@
 class Solution:
     def maxMoves(self, grid: List[List[int]]) -> int:
         
-        m, n = len(grid), len(grid[0])
-        q = set(range(m))
-        for j in range(n - 1):
+        r, c = len(grid), len(grid[0])
+
+        unique = set(range(r))
+
+        for i in range(c - 1):
             t = set()
-            for i in q:
-                for k in range(i - 1, i + 2):
-                    if 0 <= k < m and grid[i][j] < grid[k][j + 1]:
+
+            for j in unique:
+                for k in range(j - 1, j + 2):
+
+                    if 0 <= k < r and grid[j][i] < grid[k][i + 1]:
                         t.add(k)
+
             if not t:
-                return j
-            q = t
-        return n - 1
+                return i
+            
+            unique = t
+            
+        return c - 1
