2fdf305a9e90b8014d8e7fc3847ef44221bfb7a1
prodsl <imptshas@outlook.com>
2024-12-28 17:36:04 +0800

    sol 724

---
+++ Easy/724/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left, right = 0, sum(nums)
+
+        for i, x in enumerate(nums):
+            right -= x
+
+            if left == right:
+                return i
+
+            left += x
+            
+        return -1
