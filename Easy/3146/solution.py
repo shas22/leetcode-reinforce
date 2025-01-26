d9a93efc4084ddea22b5e24665c8de020f642a84
prodsl <imptshas@outlook.com>
2025-01-26 18:20:15 +0800

    sol 346

---
+++ Easy/3146/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findPermutationDifference(self, source_string: str, target_string: str) -> int:
+        char_to_position = {}
+
+        for position, character in enumerate(source_string):
+            char_to_position[character] = position
+        
+
+        total_difference = 0
+        for current_position, character in enumerate(target_string):
+            original_position = char_to_position[character]
+            total_difference += abs(original_position - current_position)
+            
+        return total_difference
